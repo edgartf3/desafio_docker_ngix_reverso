6f21c0d1598652c6c2b9731e27d0d347fbb7f9d0
root <root@DESKTOP-7NL210G.localdomain>
2022-03-10 13:53:57 -0300

    first commit

---
+++ node/index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+};
+const mysql = require('mysql');
+
+
+app.get('/', (req, res) => { 
+    const connection = mysql.createConnection(config);
+    const sql = `INSERT INTO people(name) values ('Edgar Freitas')`;
+    connection.query(sql)
+
+    connection.query("select * from people", (err, result) => {
+        console.log(result[0]["name"])
+        list = "";
+        result.forEach(element => {
+            list += `<li>${element["name"]}</li>`
+            
+        });
+        connection.end()
+        res.send("<h1>Full Cycle Rocks!</h1><br>"+list)
+    })
+        
+       
+
+})
+
+app.listen(port, () => {
+    console.log("Rodando na porta "+port)
+})
