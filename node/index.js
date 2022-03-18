6aa4405b21c3dab9b420fdfd53024f91c1fee316
Edgar Torres de Freitas <edgartorresfreitas@gmail.com>
2022-03-18 14:53:38 -0300

    ajsute

--- node/index.js
+++ node/index.js
@@ -12,35 +12,38 @@ const mysql = require('mysql');
 
 
 app.get('/', (req, res) => { 
-    //res.send("<h1>Full Cycle Rocks!</h1><br>")
-
     const connection = mysql.createConnection(config);
+
     const sql = `INSERT INTO people(name) values ('Edgar Freitas')`;
-    connection.query(sql)
-
-    connection.query("select * from people", (err, result) => {
-        console.log(result[0]["name"])
-        list = "";
-        result.forEach(element => {
-            list += `<li>${element["name"]}</li>`
-            
-        });
-        connection.end()
-        res.send("<h1>Full Cycle Rocks!</h1><br>"+list)
+    connection.query(sql, (err, result) => {
+        connection.query("select * from people", (err, result) => {
+            console.log(result[0]["name"])
+            list = "";
+            result.forEach(element => {
+                list += `<li>${element["name"]}</li>`                
+            });
+            connection.end()
+            res.send("<h1>Full Cycle Rocks!</h1><br>"+list)
+        })
     })
-       
-       
 
+
+   
 })
 
 const connection = mysql.createConnection(config);
-
 const createTable = `create table IF NOT EXISTS people(id int not null auto_increment, name varchar(255), primary key(id)) `;
 connection.query(createTable,  (err, result) => {
     console.log("Tabela criada")
-    app.listen(port, () => {
-        console.log("Rodando na porta "+port)
-    })
+    const sql = `INSERT INTO people(name) values ('Edgar Freitas')`;
+    connection.query(sql, (err, result) => {
+        app.listen(port, () => {
+            console.log("Rodando na porta "+port)
+        })
+    })                   
 })
+
+
+
 
 
