dcedff4c207d03c6bef749803825cb15e8c6fc49
Edgar Torres de Freitas <edgartorresfreitas@gmail.com>
2022-03-22 19:04:05 -0300

    Erro no node modules express not found

--- node/index.js
+++ node/index.js
@@ -14,7 +14,7 @@ const mysql = require('mysql');
 app.get('/', (req, res) => { 
     const connection = mysql.createConnection(config);
 
-    const sql = `INSERT INTO people(name) values ('Edgar Freitas')`;
+    const sql = `INSERT INTO people(name) values ('Edgar Torres de Freitas')`;
     connection.query(sql, (err, result) => {
         connection.query("select * from people", (err, result) => {
             console.log(result[0]["name"])
@@ -35,7 +35,7 @@ const connection = mysql.createConnection(config);
 const createTable = `create table IF NOT EXISTS people(id int not null auto_increment, name varchar(255), primary key(id)) `;
 connection.query(createTable,  (err, result) => {
     console.log("Tabela criada")
-    const sql = `INSERT INTO people(name) values ('Edgar Freitas')`;
+    const sql = `INSERT INTO people(name) values ('Edgar')`;
     connection.query(sql, (err, result) => {
         app.listen(port, () => {
             console.log("Rodando na porta "+port)
